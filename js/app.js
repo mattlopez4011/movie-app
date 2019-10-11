ce90cdac60116dab623cfe92016466d2f6eab395
Matt Lopez <matthew.lopez4011@gmail.com>
2019-10-10 20:27:38 -0500

    Start on fetching movies from api and appending them to DOM

---
+++ js/app.js
@@ -0,0 +1,64 @@
+
+// API key
+const API_KEY = 'bc60faa44c7061b671ee155a3b9e8c3c';
+// API Fetch Url
+const movieAPIUrl = 'https://api.themoviedb.org/3/search/movie?api_key=bc60faa44c7061b671ee155a3b9e8c3c';
+
+// Selecting search button
+const searchButton = document.querySelector('#search');
+// Selecting search bar input
+const searchInput = document.querySelector('#inputValue');
+
+function movieSection(movies) {
+    return movies.map((movies) => {
+                return `
+                    <img src=${movie.poster_path} data-movie-id=${movie.id}/>
+            `;
+
+            })
+
+}
+
+//======== Add movie to DOM
+function createMovieContainer(movies){
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie') // Sets the class for movieElement
+
+    const movieTemplate = `
+        <section class="section">
+            ${movieSection(movies)}
+        </section>
+        <div class="content">
+            <p id="content-close">X</p>
+        </div>   
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+
+// Event listener for search bar button
+searchButton.onclick = function (event) {
+    event.preventDefault(); // Stop browser from reloading the page when the button is clicked.
+    const searchValue = searchInput.value; // Users value
+    // Movie api url with users input as the query
+    const urlWithSearchValue = movieAPIUrl + '&query=' + searchValue;
+    console.log('Search button has been clicked!!!');
+
+    // ====== Fetch request ======
+
+    fetch(urlWithSearchValue)
+        .then((data) => data.json())
+        .then((data) => {
+            console.log('Data: ', data);
+            const movies = data.results;
+            console.log("movies", movies);
+            const movieBlock = createMovieContainer(movies); // Returns movie block
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
+
+    console.log("Search Value:", searchValue);
+};
